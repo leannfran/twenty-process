d4078c90dadc5eab52299fbe6fe6212267a6556a
Alan Vargas <112961710+AlanEzequielVargas@users.noreply.github.com>
2023-06-27 15:37:16 -0300

    feat: directories organized and initial dependencies installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
+    './index.html', './src/**/*.{vue,js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors:{
-        primary: '#47A29C'
-      }
-   
+      colors: {
+        primary: "#47A29C",
+      },
+    },
+    dimensions: {
+      xxl: "1440px",
     },
   },
   plugins: [],
-}
+});
