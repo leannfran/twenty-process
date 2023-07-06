83b2660f47655299d1b5086b65406fd133f16acf
Alan Vargas <112961710+AlanEzequielVargas@users.noreply.github.com>
2023-07-06 14:26:30 -0300

    refactor: all swipers are merged in one

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,59 +1,32 @@
-import React from 'react'
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import {FaArrowRight} from 'react-icons/fa';
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import CardProduct from '../components/cards/CardProduct';
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import CardSwiper from "./Swiper";
 
 const Productos = () => {
+  return (
+    <>
+     
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="text-black text-lg py-3 pl-4 lg:pl-0">
+              PRODUCTOS DESTACADOS{" "}
+            </h2>
+            <hr className="w-28 border border-primary ml-4  lg:ml-0" />
+          </div>
+          <div>
+            <h2 className="text-black text-lg font-bold items-center gap-5 hidden md:flex mr-2">
+              Ver más productos <FaArrowRight />
+            </h2>
+          </div>
+        </div>
 
-    const productCatalogue = 5;
+        <CardSwiper vertical={false} swiperClass="products" length={2.3} />
+        <h2 className="md:hidden text-right text-black text-lg font-bold flex items-center justify-end gap-5 mr-2">
+          Ver más productos <FaArrowRight />
+        </h2>
+      
+    </>
+  );
+};
 
-    return (
-        <>
-
-            <div>
-
-                <div className='flex justify-between items-center'>
-                    <div>
-                        <h1 className='text-black text-lg font-bold py-3 text-left uppercase'>Productos destacados</h1>
-                        <hr className="w-[10rem] border border-primary" />
-                    </div>
-                    <div>
-
-                        <h2 className='text-black text-lg font-bold flex items-center gap-5'>Ver más productos <FaArrowRight/></h2>
-                        
-                    </div>
-                </div>
-
-                <Swiper className="mySwiper"
-                    spaceBetween={70}
-                    direction="horizontal"
-                    slidesPerView={2.7}
-                    pagination={{ clickable: true }}
-                    loop={true}
-                >
-
-                    {[...Array(productCatalogue)].map((e, i) => (
-                        <SwiperSlide key={i}>
-                            <CardProduct />
-                        </SwiperSlide>
-                    ))}
-
-
-                </Swiper>
-
-            </div>
-
-
-        </>
-    )
-}
-
-export default Productos
+export default Productos;
--- src/components/cards/CardProduct.jsx
+++ src/components/cards/CardProduct.jsx
@@ -1,37 +1,39 @@
-import React from 'react'
+import React from "react";
 import productImg from "../../../public/assets/Head.png";
-import Image from 'next/image';
+import Image from "next/image";
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    Typography,
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
 } from "@material-tailwind/react";
 
 const Productos = () => {
+  return (
+    <>
+      <Card className="w-80 h-80 md:w-96 m-auto">
+        <CardHeader color="blue-gray" className="relative h-56 m-0 rounded-b-none shadow-none">
+          <Image
+            src={productImg}
+            width={500}
+            height={500}
+            alt="Producto Imagen"
+            className="object-cover rounded-b-none"
+          />
+        </CardHeader>
+        <CardBody className="p-3 ">
+          <Typography variant="h5" color="blue-gray" className=" text-left">
+            Nombre del producto
+          </Typography>
+          {/* <Typography className="text-left">
+            The place is close to Barceloneta Beach and bus stop just 2 min by
+            walk and near to &quot;Naviglio&quot; where you can enjoy the main
+            night life in Barcelona.
+          </Typography> */}
+        </CardBody>
+      </Card>
+    </>
+  );
+};
 
-    return (
-        <>
-
-            <Card className="mt-6 w-96 py-5 mb-0">
-                <CardHeader color="blue-gray" className="relative h-56">
-                    <Image src={productImg} width={500} height={500} alt='Producto Imagen' className='object-cover'/>
-                </CardHeader>
-                <CardBody>
-                    <Typography variant="h5" color="blue-gray" className="mb-2 text-left">
-                        Nombre del producto
-                    </Typography>
-                    <Typography className='text-left'>
-                        The place is close to Barceloneta Beach and bus stop just 2 min by walk
-                        and near to &quot;Naviglio&quot; where you can enjoy the main night life
-                        in Barcelona.
-                    </Typography>
-                </CardBody>
-            </Card>
-
-
-        </>
-    )
-}
-
-export default Productos
+export default Productos;
