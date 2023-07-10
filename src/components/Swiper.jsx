6335b53826e6b8c8b2d8c1004a531553824f2659
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-07-10 12:08:29 -0300

    style(frontend): Modificando swiper js

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -11,6 +11,8 @@ import Image from "next/image";
 import brandLogo from "../../public/assets/slazenger-logo.png";
 import CardProduct from "./cards/CardProduct";
 import CategoriesProduct from "./CategoriesProduct";
+import { useEffect } from "react";
+
 
 export default function CardSwiper({
   openCatalogue,
@@ -23,67 +25,67 @@ export default function CardSwiper({
 
   return (
     <Swiper
-      className={`my-10 ${
-        swiperClass === "catalogues"
+      className={`my-10 ${swiperClass === "catalogues"
           ? "h-[350px] md:h-[800px]"
           : swiperClass === "products"
-          ? "h-[330px]"
-          : "h-[210px]"
-      }`}
+            ? "h-[330px]"
+            : "h-[210px]"
+        }`}
       direction={vertical ? "vertical" : "horizontal"}
       pagination={{ clickable: true }}
       loop={true}
       mousewheel={true}
       autoplay={autoplay ? { delay: 3000 } : false}
       modules={[Autoplay, Navigation]}
+      spaceBetween={swiperClass == "categories" ? 90 : null}
       breakpoints={{
         0: {
           slidesPerView:
             swiperClass === "catalogues"
               ? 1.1
               : swiperClass === "products"
-              ? 1.1
-              : swiperClass === "categories"
-              ? 2.3
-              : 2,
+                ? 1.1
+                : swiperClass === "categories"
+                  ? 2.3
+                  : 2,
         },
         400: {
           slidesPerView:
             swiperClass === "catalogues"
               ? 1.2
               : swiperClass === "products"
-              ? 1.2
-              : swiperClass === "categories"
-              ? 2.5
-              : 2,
+                ? 1.2
+                : swiperClass === "categories"
+                  ? 2.5
+                  : 2,
         },
         568: {
           slidesPerView:
             swiperClass === "catalogues"
               ? 1.4
               : swiperClass === "products"
-              ? 1.4
-              : swiperClass === "categories"
-              ? 3.3
-              : 2,
+                ? 1.4
+                : swiperClass === "categories"
+                  ? 3.3
+                  : 2,
         },
         630: {
           slidesPerView:
             swiperClass === "catalogues"
               ? 1.8
               : swiperClass === "products"
-              ? 1.8
-              : swiperClass === "categories"
-              ? 4.2
-              : 2,
+                ? 1.8
+                : swiperClass === "categories"
+                  ? 4.2
+                  : 2,
         },
         720: {
           slidesPerView:
             swiperClass === "products"
               ? 1.8
               : swiperClass === "categories"
-              ? 4.2
-              : length,
+                ? 4.2
+                : length,
         },
         918: {
           slidesPerView: length,
@@ -92,31 +94,33 @@ export default function CardSwiper({
     >
       {swiperClass === "catalogues"
         ? [...Array(cataloguesTest)].map((e, i) => (
-            <SwiperSlide key={i}>
-              <CardCatalogue openCatalogue={openCatalogue} />
-            </SwiperSlide>
-          ))
+          <SwiperSlide key={i}>
+            <CardCatalogue openCatalogue={openCatalogue} />
+          </SwiperSlide>
+        ))
         : swiperClass === "logos"
-        ? [...Array(cataloguesTest)].map((e, i) => (
+          ? [...Array(cataloguesTest)].map((e, i) => (
             <SwiperSlide key={i}>
               <div className="px-12 w-30 md:w-60 ">
                 <Image src={brandLogo} alt="logo" />
               </div>
             </SwiperSlide>
           ))
-        : swiperClass === "products"
-        ? [...Array(cataloguesTest)].map((e, i) => (
-            <SwiperSlide key={i}>
-              <CardProduct />
-            </SwiperSlide>
-          ))
-        : swiperClass === "categories"
-        ? [...Array(cataloguesTest)].map((e, i) => (
-            <SwiperSlide key={i}>
-              <CategoriesProduct />
-            </SwiperSlide>
-          ))
-        : null}
+          : swiperClass === "products"
+            ? [...Array(cataloguesTest)].map((e, i) => (
+              <SwiperSlide key={i}>
+
+                <CardProduct />
+
+              </SwiperSlide>
+            ))
+            : swiperClass === "categories"
+              ? [...Array(cataloguesTest)].map((e, i) => (
+                <SwiperSlide key={i}>
+                  <CategoriesProduct />
+                </SwiperSlide>
+              ))
+              : null}
     </Swiper>
   );
 }
