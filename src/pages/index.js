1a0d9b3971c01357d0d31d2d31362c33734ec9b9
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-07-14 19:57:43 -0300

    style(index): Modificando box shadow cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,7 +63,7 @@ export default function Home() {
             />
           </div>
           <div
-            style={{ boxShadow: "1px -65px 48px -31px rgba(0,0,0,0.75) inset" }}
+            style={{ boxShadow: "1px -65px 48px -31px rgba(255,255,255,0.75) inset" }}
             className="w-full h-28 absolute bottom-0 z-10 hidden md:block"
           ></div>
           {showCatalogue && (
