b4d07b471b1bb6f9c60bee0ac20f8d442f7afeae
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-08-01 15:30:10 -0300

    feat(Categories): Agregando popover al icono de categorias

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 import CardSwiper from "./Swiper";
 import Link from "next/link";
 import {MdOutlineSwipe} from 'react-icons/md'
+import { Popover, PopoverContent, PopoverHandler } from "@material-tailwind/react";
 
 const Categories = () => {
+
+  const [openPopover, setOpenPopover] = useState(false)
+
+  const triggers = {
+
+    onMouseEnter: () => setOpenPopover(true),
+    onMouseLeave: () => setOpenPopover(false),
+
+  };
 
   return (
     <>
       <div className="flex py-10 justify-between items-center">
         <div className="md:ml-24">
-          <h2 className="text-black text-lg  flex gap-2 items-center py-3 pl-4 lg:pl-0">CATEGORÍAS <MdOutlineSwipe className=" animate-infinite animate-wiggle-more"/> </h2>
+          <h2 className="text-black text-lg  flex gap-2 items-center py-3 pl-4 lg:pl-0">CATEGORÍAS 
+          
+            <Popover
+              placement="right-end"
+              open={openPopover}
+              handler={setOpenPopover}
+            >
+              <PopoverHandler {...triggers}>
+                <span>
+                  <MdOutlineSwipe className=" animate-infinite animate-wiggle-more cursor-help" />
+                </span>
+              </PopoverHandler>
+              <PopoverContent {...triggers}>
+                <span>Desliza de izquierda a derecha.</span>
+              </PopoverContent>
+            </Popover>
+
+           </h2>
           <hr className="w-28 border border-primary ml-4 lg:ml-0" />
         </div>
         <div>
