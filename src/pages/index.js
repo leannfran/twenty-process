6852435813fc24c39e5cfbce677141c2c86bf2b1
Alan Vargas <112961710+AlanEzequielVargas@users.noreply.github.com>
2023-09-13 11:44:13 -0300

    feat: loader in cards added

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,21 @@ import Categories from "../components/Categories";
 import ContactForm from "../components/ContactForm";
 import SpeedDial from "../components/SpeedDial";
 import { Layout } from "@/components/layout/Layout";
-import {MdOutlineSwipeVertical} from "react-icons/md"
-import { Popover, PopoverContent, PopoverHandler } from "@material-tailwind/react";
+import { MdOutlineSwipeVertical } from "react-icons/md";
+import {
+  Popover,
+  PopoverContent,
+  PopoverHandler,
+} from "@material-tailwind/react";
+
+
 export default function Home() {
   const [showCatalogue, setShowCatalogue] = React.useState(false);
-  const [openPopover, setOpenPopover] = useState(false)
+  const [openPopover, setOpenPopover] = useState(false);
 
   const triggers = {
-
     onMouseEnter: () => setOpenPopover(true),
     onMouseLeave: () => setOpenPopover(false),
-
   };
 
   function handleOpenCatalogue(open) {
@@ -40,7 +44,10 @@ export default function Home() {
           className=" animate-fade
  font-light text-gray-900 text-center p-4 md:p-10 text-xl md:text-4xl"
         >
-        <span className="font-semibold text-primary">MERCHANDISING </span> PERSONALIZADO PARA <span className="font-semibold text-primary">POTENCIAR </span> TU MARCA
+          <span className="font-semibold text-primary">MERCHANDISING </span>{" "}
+          PERSONALIZADO PARA{" "}
+          <span className="font-semibold text-primary">POTENCIAR </span> TU
+          MARCA
         </h1>
 
         <Productos />
@@ -50,7 +57,7 @@ export default function Home() {
           id="catalogues"
         >
           <h2 className="text-black flex gap-4 text-lg py-3 pl-4 lg:pl-0">
-            DESCARGÁ NUESTROS CATÁLOGOS 
+            DESCARGÁ NUESTROS CATÁLOGOS
             <Popover
               placement="right-end"
               open={openPopover}
@@ -58,7 +65,7 @@ export default function Home() {
             >
               <PopoverHandler {...triggers}>
                 <span>
-                <MdOutlineSwipeVertical className=" animate-wiggle-more animate-infinite cursor-help"/>
+                  <MdOutlineSwipeVertical className=" animate-wiggle-more animate-infinite cursor-help" />
                 </span>
               </PopoverHandler>
               <PopoverContent {...triggers}>
@@ -67,8 +74,8 @@ export default function Home() {
             </Popover>
           </h2>
           <hr className="w-28 border border-primary ml-4 lg:ml-0" />
-          
-                    <div className="">
+
+          <div className="">
             <CardSwiper
               openCatalogue={handleOpenCatalogue}
               vertical={false}
