bd651057e0ea3b63d34cd51bfd89d211bffb1543
Leandro <leandrofranco50@yahoo.com.ar>
2023-09-25 09:46:53 -0300

    Update Productos.jsx

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -23,9 +23,8 @@ const Productos = () => {
         <div className="flex justify-between items-center" id="productos">
           <div className="md:ml-24">
             <h2 className="text-black  flex gap-3 items-center text-lg py-3 pl-4 lg:pl-0">
-              PRODUCTOS DESTACADOS{" "} <br />
-            Estamos Actualizando Nuestros productos, Contactenos por WhatsApp tocando <a           href="https://api.whatsapp.com/send?phone=5491178311503&text=%C2%A1Hola!,%20Me%20interesa%20conocer%20m%C3%A1s%20sobre%20los%20servicios%20que%20ofrece%20Twenty,%20%C2%BFPodr%C3%ADan%20proporcionarme%20m%C3%A1s%20informaci%C3%B3n%20y%20precios?%20%C2%A1Gracias!%20%F0%9F%98%80"
->Aca</a>
+              PRODUCTOS DESTACADOS{" "}
+  
 
             <Popover
               placement="right-end"
@@ -43,7 +42,12 @@ const Productos = () => {
             </Popover>
 
             </h2>
+
             <hr className="w-28 border border-primary ml-4  lg:ml-0" />
+           <p>
+             Estamos Actualizando Nuestros productos, Contactenos por WhatsApp tocando <a   className=" font-bold overline text-primary"        href="https://api.whatsapp.com/send?phone=5491178311503&text=%C2%A1Hola!,%20Me%20interesa%20conocer%20m%C3%A1s%20sobre%20los%20servicios%20que%20ofrece%20Twenty,%20%C2%BFPodr%C3%ADan%20proporcionarme%20m%C3%A1s%20informaci%C3%B3n%20y%20precios?%20%C2%A1Gracias!%20%F0%9F%98%80"
+>Aca</a>
+             </p> 
           </div>
 
           <div>
