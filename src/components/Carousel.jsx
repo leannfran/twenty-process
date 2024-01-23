90e36097717de6113939580fbf485ef4133da6ed
Leandro <leandrofranco50@yahoo.com.ar>
2024-01-23 10:19:25 -0300

    img

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,7 +2,7 @@ import { Carousel } from "@material-tailwind/react";
 import carouselImg from "../../public/assets/9.png";
 import carouselFirst from "../../public/assets/7.png";
 import carouselSecond from "../../public/assets/8.png";
-import cole from "../../public/assets/cole.png";
+import carouselNavidad from "../../public/assets/11.png";
 
 
 import Image from "next/image";
@@ -35,8 +35,8 @@ export default function CarouselHero() {
         className="mySwiper text-black"
 
       >
-          <SwiperSlide>  <Image
-          src={cole}
+          <SwiperSlide>  <img
+          src={carouselNavidad}
           alt="image vueta al cole"
           className="h-full w-full object-cover"
           priority
